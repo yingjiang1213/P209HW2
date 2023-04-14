f141062c1474a60fa30b60ccb04eb1ff2a5b7386
yingjiang1213 <jy229319791@hotmail.com>
2023-04-14 14:19:37 -0700

    The project works now, but it still needs some modifications.

---
+++ pizza.js
@@ -0,0 +1,32 @@
+var sizePrice=new Array();
+sizePrice['small']=7;
+sizePrice['medium']=10;
+sizePrice['large']=14;
+
+function getSizePrice(){
+  var pizzaSizePrice=0;
+  var form= document.forms['orderform'];
+  var checkedSize=form.elements['size'];
+  pizzaSizePrice+=sizePrice[checkedSize.value];
+  return pizzaSizePrice;
+}
+
+
+var toppingPrice=new Array();
+toppingPrice['noToppings']=0;
+toppingPrice['oneTopping']=2;
+toppingPrice['twoToppings']=3;
+
+function getToppingPrice(){
+    var pizzaToppingPrice=0;
+    var form= document.forms['orderform'];
+    var selectedTopping=form.elements['toppings'];
+    pizzaToppingPrice+=toppingPrice[selectedTopping.value];
+    return pizzaToppingPrice;
+}
+
+
+function getTotal(){
+    var total= getSizePrice()+getToppingPrice();
+    alert('Total:'+total);
+}
